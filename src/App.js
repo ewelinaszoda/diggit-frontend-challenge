ae69b4da5d9b8e738ce9c7d4a79ac0f856016813
ewelinaszoda <ewelinaszoda@gmail.com>
2020-10-05 17:34:55 +0100

    Fix  bugs in Graphs file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import Graphs from "./components/Graphs";
-import stats from "./components/common/stats";
 
 import Homepage from "./screens/Homepage/Homepage";
 
